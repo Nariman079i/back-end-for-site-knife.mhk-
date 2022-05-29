a1c1018b674fcd27a57200117145a6bd302880f7
Nariman <Nariman079i>
2022-05-29 10:51:24 +0300

    Создание события для кнопки заказа

---
+++ KnifeMhk/main/static/main/js/order.js
@@ -0,0 +1,6 @@
+function clickBtn(){
+    var name_f = document.getElementById('name').value;
+    var num_f = document.getElementById('phone').value;
+    var adress_f = document.getElementById('adress').value;
+    console.log(name_f);
+}
