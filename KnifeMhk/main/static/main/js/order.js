ea28d28d9b3f48aaeebcdea51fc048cfbd92bec3
Nariman <Nariman079i>
2022-05-29 11:09:28 +0300

    Создание события для кнопки заказа 1

--- KnifeMhk/main/static/main/js/order.js
+++ KnifeMhk/main/static/main/js/order.js
@@ -2,5 +2,5 @@ function clickBtn(){
     var name_f = document.getElementById('name').value;
     var num_f = document.getElementById('phone').value;
     var adress_f = document.getElementById('adress').value;
-    console.log(name_f);
+    window.location.href = "http://stackoverflow.com";
 }
