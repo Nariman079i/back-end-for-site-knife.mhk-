71b66734d1320f3b958169d9b37ed0dad01d3935
Nariman <Nariman079i>
2022-10-12 17:05:01 +0300

    get category knives 4

--- KnifeMhk/main/views.py
+++ KnifeMhk/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import *
+
 # Create your views here.
+
+
 def base(request):
     types_ = Type.objects.all()
     posts = Knife.objects.all()
-    return render(request, 'main/base.html' , context={'posts':posts , 'types' : types_})
+    return render(request, 'main/base.html' , context={'posts':posts , 'types' : types_ })
+
